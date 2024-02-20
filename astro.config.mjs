ef18cd3a2129f3146454ef6e5fae86a6e60d0726
Jonas <publik.froeller.jonas@gmx.at>
2024-02-20 16:00:47 +0100

    added analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,37 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Propromo Docs',
-			favicon: './public/favicon.png',
-			logo: {
-				src: './public/favicon.png',
-			},
-			social: {
-				github: 'https://github.com/propromo-software/propromo',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Joining a Project Dahboard', link: '/guides/join-dashboard/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'Propromo Docs',
+    favicon: './public/favicon.png',
+    logo: {
+      src: './public/favicon.png'
+    },
+    social: {
+      github: 'https://github.com/propromo-software/propromo'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Joining a Project Dahboard',
+        link: '/guides/join-dashboard/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  })],
+  output: "hybrid",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
