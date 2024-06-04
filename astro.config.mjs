2115b0d96e32223dbdb9ca16df8b6d891f2781f3
Jonas <publik.froeller.jonas@gmx.at>
2024-06-04 21:47:02 +0200

    search(seo): added `@astrojs/sitemap`

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://propromo-docs.vercel.app',
   integrations: [starlight({
     title: 'Propromo Docs',
     favicon: '/img/favicon.png',
